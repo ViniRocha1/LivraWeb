b04cfd72385ddc253111901cea9243232966318e
ViniRocha1 <vinicius.barros@sptech.school>
2025-05-28 13:45:37 -0300

    commitando api cadastro, login e banco

--- LivraWeb/src/database/script-tabelas.sql
+++ LivraWeb/src/database/script-tabelas.sql
@@ -6,63 +6,9 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
-
 -- criar banco de dados livraweb
 create database livraweb;
+drop database livraweb;
 
 -- selecionar o banco de dados
 use livraweb;
@@ -74,7 +20,6 @@ create table usuario (
     nome varchar(100),
     email varchar(100),
     senha varchar(100),
-    data_cadastro date,
     primary key (id)
 );
 
@@ -90,15 +35,22 @@ create table manhwa (
 );
 
 
+
+insert into manhwa (titulo, autor, genero, sinopse, data_publicacao) values ('The Beggening After The End', 'Brandon Lee', 'Ação', 'O rei Grey, apesar de um reinado próspero, 
+sente um vazio interior e, após morrer, reencarna como Arthur Leywin, um bebê em um mundo mágico. 
+Arthur, com a consciência da sua vida anterior, busca dominar a magia e se tornar um poderoso guerreiro.', '2018-07-07' );
+
+
 -- criar tabela de quiz
 create table quiz (
     id int auto_increment,
     titulo varchar(150),
     id_manhwa int,
-    total_perguntas int,
     foreign key (id_manhwa) references manhwa(id),
     primary key (id)
 );
+
+insert into quiz (titulo, id_manhwa) values('Quiz TBATE', 1 );
 
 -- criar tabela de pergunta
 create table pergunta (
@@ -130,3 +82,5 @@ create table resposta_usuario (
     foreign key (id_alternativa) references alternativa(id),
     primary key (id_usuario, id_pergunta)
 );
+
+
